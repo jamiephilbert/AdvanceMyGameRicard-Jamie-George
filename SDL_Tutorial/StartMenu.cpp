71d192dd8140b6abc4c2dfb16457fd73c762e331
jamiephilbert <116039576+jamiephilbert@users.noreply.github.com>
2022-11-17 23:51:25 -0500

    Updating Start Menu

--- SDL_Tutorial/StartMenu.cpp
+++ SDL_Tutorial/StartMenu.cpp
@@ -4,3 +4,15 @@ StartMenu::StartMenu() {
 	m_pTopBar = new GameEntity(Graphics::SCREEN_WIDTH * 0.5f, 80.0f);
 	m_pHiScore = new Texture("Hi Score", "ARCADE.TTF", 18, { 230, 230, 230 });
 }
+
+StartMenu::~StartMenu()
+{
+}
+
+void StartMenu::Update()
+{
+}
+
+void StartMenu::Render()
+{
+}
